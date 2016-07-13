83a8ec84f631c8f2da9d863812f016df4f405291
Charles Bouley <charlesdavidbouley@gmail.com>
2016-07-12 22:55:54 -0400

    20160712 Python Updates

--- learn_python/ex11.py
+++ learn_python/ex11.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2.7
 
+# Ending each print line with a , (comma) stops print from ending the line with a newline character
+# raw_input() returns a string and input() tries to run the input as a Python expression
 print "How old are you?",
 age = raw_input()
 print "How tall are you?",
@@ -7,5 +9,10 @@ height = raw_input()
 print "How much do you weigh?",
 weight = raw_input()
 
-print "So, you're %r old, %r tall and %r heavy." % (
-		age, height, weight)
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+		
+# Getting numbers
+# x = int(raw_input()) - This will get the number as a string form raw_input() 
+# then convert it to an integer using int()
+
+# Python lines should be less than 80 characters long.
---
+++ learn_python/ex12.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+### IMPORTANT ### %r is fo debugging (raw representation) and %s is for display.
